8f60f616e80292e262c7f293d0c94569527eddeb
Natalia Django <potockaja1990@gmail.com>
2018-04-28 17:12:20 +0300

    blog

--- blog/app/views.py
+++ blog/app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
+
+def post_list(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'app/post_list.html',{"posts": posts} )
+
+
+def post_detail(request, pk):
+    post=get_object_or_404(Post, pk=pk)
+    return render(request, 'app/post detail.html',{"post": post} )
 
 # Create your views here.
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,3 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
